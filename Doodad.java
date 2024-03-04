5b89ef77279aed17396a149cf7e183cc228eddfc
christinakam <144845620+christinakam@users.noreply.github.com>
2024-03-03 21:12:54 -0800

    Update Doodad.java

--- Doodad.java
+++ Doodad.java
@@ -1,10 +1,12 @@
-class Doodad
-{
+class Doodad{
   private double myNum;
   public Doodad()
   {
     myNum = 0;
   }
-  public double getNum(){/* your java code */}
-  public void setNum(double n){/* your java code */}
+  public double getNum(){
+return myNum}
+  public void setNum(double n){
+    myNum = n;
+  }
 }
